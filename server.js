b01144f7ff0aea647979827143c18145b21c5fbb
iqbalfaisal <7faisaliqbal@gmail.com>
2020-11-26 11:19:30 +0500

    bodyParser

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log("We are live on " + port);
 });
